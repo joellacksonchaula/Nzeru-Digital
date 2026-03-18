a9bff0d4a9cf65960289e6dccb6a9df2961859bf
Joel Ndege <ndegejoel2000@gmail.com>
2026-03-18 22:55:39 +0200

    fix: add python-dotenv

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -4,10 +4,11 @@ from pathlib import Path
 from datetime import timedelta
 
 import dj_database_url
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
+try:
+    from dotenv import load_dotenv
+    load_dotenv()
+except:
+    pass
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 logger = logging.getLogger(__name__)
